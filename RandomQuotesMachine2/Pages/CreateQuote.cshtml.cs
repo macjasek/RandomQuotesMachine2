73bea58264fab9ed61f64a0a723fdca2a603e16f
macjasek <maciej.dobruchowski@ispro.com.pl>
2018-01-15 20:00:30 +0100

    Adding CreateQuotes page first element of CRUD functionality

---
+++ RandomQuotesMachine2/Pages/CreateQuote.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RandomQuotesMachine2.Models;
+using System.Threading.Tasks;
+
+namespace RandomQuotesMachine2.Pages
+{
+    public class CreateQuoteModel : PageModel
+    {
+        private readonly QuotesDbContext _db;
+
+        public CreateQuoteModel(QuotesDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Quotes Quotes { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Quotes.Add(Quotes);
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
