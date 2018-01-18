d381e5cf483b916af41f0d136b27e2eccd683eae
macjasek <maciej.dobruchowski@ispro.com.pl>
2018-01-18 20:52:23 +0100

    Modification of index page model to get the random quote. The result is not exactly what I need but I think that I know how I should make it.

--- RandomQuotesMachine2/Pages/Index.cshtml.cs
+++ RandomQuotesMachine2/Pages/Index.cshtml.cs
@@ -1,17 +1,46 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RandomQuotesMachine2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RandomQuotesMachine2.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly QuotesDbContext _db;
 
+        public IndexModel(QuotesDbContext db)
+        {
+            _db = db;
+        }
+
+        
+
+
+        [BindProperty]
+        public Quotes Quote { get; set; }
+        [BindProperty]
+        public IList<Quotes> QuoteList { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(long id)
+        {
+            var rndGen = new Random();
+            QuoteList = await _db.Quotes.AsNoTracking().ToListAsync();
+            int random = rndGen.Next(0, QuoteList.Count);
+            id = QuoteList.ElementAt(random).Id;
+
+            Quote = await _db.Quotes.FindAsync(id);
+            if (Quote == null)
+            {
+                
+                return RedirectToPage($"/{id}");
+            }
+
+            return Page();
         }
     }
 }
