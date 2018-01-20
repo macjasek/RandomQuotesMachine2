9c48a48ce143f5c12886c59b73b57abdafb7cd25
macjasek <maciej.dobruchowski@ispro.com.pl>
2018-01-20 17:15:06 +0100

    Styling with CSS to get a better look and keep the quote in the container with FB image aspect ratio.

--- RandomQuotesMachine2/Models/Quotes.cs
+++ RandomQuotesMachine2/Models/Quotes.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace RandomQuotesMachine2.Models
 {
     public partial class Quotes
     {
         public long Id { get; set; }
+        [Required]
         public string Qoute { get; set; }
+        [Required]
         public string Author { get; set; }
         public int IsSelected { get; set; }
     }
