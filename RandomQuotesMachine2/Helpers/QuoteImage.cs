8394e4417f7470af69eeaf79ca71eb64983ee268
macjasek <maciej.dobruchowski@ispro.com.pl>
2018-01-25 20:55:33 +0100

    A package has been added that supports the creation of a quote drawing. Functions have been added to calculate the size of one line of text and the number of rows for the entire quote.

--- RandomQuotesMachine2/Helpers/QuoteImage.cs
+++ RandomQuotesMachine2/Helpers/QuoteImage.cs
@@ -1,11 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using RandomQuotesMachine2.Models;
+using System.Drawing;
 
 namespace RandomQuotesMachine2.Helpers
 {
     public class QuoteImage
     {
+
+        public static void Create(Quotes quote, string fontName, int fontSize, float maximumTextSize)
+        {
+            Font font = new Font(fontName, fontSize, FontStyle.Regular, GraphicsUnit.Pixel);
+
+            var textRows = CalculateNumberOfRows(quote.Qoute, font, maximumTextSize);
+
+            var img = new Bitmap(800, 416);
+            var drawing = Graphics.FromImage(img);
+            var backColor = new Color();
+            backColor = Color.FromArgb(255, 255, 255);
+            drawing.Clear(backColor);
+
+            Color textColor = Color.Black;
+
+            var totalQuoteSize = CalculateTextSize(quote.Qoute, font);
+
+            float quoteTextRowHeight = totalQuoteSize.Height * 1.25f;
+            float quoteRectangleHeight = quoteTextRowHeight * textRows;
+
+
+
+            for (int i = 0; i < textRows; i++)
+            {
+                //draw text on image
+            }
+
+
+            img.Dispose();
+            drawing.Dispose();
+        }
+
+        private static int CalculateNumberOfRows(string quoteText, Font font, float maximumTextSize)
+        {
+            int numberOfRows = 1;
+
+            string[] words = quoteText.Split(' ');
+
+            string tempQuote = "";
+
+            foreach (string word in words)
+            {
+                if (tempQuote == "")
+                {
+                    tempQuote = word;
+                }
+                else
+                {
+                    tempQuote = $"{tempQuote} {word}";
+                }
+
+                SizeF textSize = CalculateTextSize(tempQuote, font);
+
+                if (textSize.Width >= maximumTextSize)
+                {
+                    numberOfRows++;
+                    tempQuote = "";
+                }
+            }
+
+
+            return numberOfRows;
+        }
+
+        private static SizeF CalculateTextSize(string text, Font font)
+        {
+            Image image = new Bitmap(1, 1);
+            Graphics drawing = Graphics.FromImage(image);
+
+            SizeF textSize = drawing.MeasureString(text, font);
+
+            image.Dispose();
+            drawing.Dispose();
+
+            return textSize;
+        }
+
+        
     }
 }
